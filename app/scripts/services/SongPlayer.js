bee8895ebbd2c2706bb5208966b3aa38d58f649c
Pedro Urbina <pedro.urbina@gmail.com>
2016-03-12 19:32:01 -0500

    checkpoint 7 assignment

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,70 @@
+(function(){
+	function SongPlayer(){
+		var SongPlayer = {};
+
+		/**
+		@desc song object, contains audioUrl attribute & more
+		@type {Obj}
+		*/
+		var currentSong = null;
+		/**
+		@desc Buzz object sound file
+		@type {Obj}
+		*/
+		var currentBuzzObject = null;
+
+		/**
+		@function setSong
+		@desc Stops currently playing song, loads new audio file as currentBuzzObject
+		@param {Obj} song
+		*/
+
+		var setSong = function(song){
+			if (currentBuzzObject){
+				currentBuzzObject.stop();
+				currentSong.playing = null;
+			}
+
+			currentBuzzObject = new buzz.sound(song.audioUrl, {
+				formats: ['mp3'],
+				preload: true
+			});
+
+			currentSong = song;
+
+		};
+
+		/**
+		@function playSong
+		@desc plays current Buzz object, sets playing attribute
+		@param {Obj} song
+		*/
+
+		var playSong = function(song){
+			currentBuzzObject.play();
+			song.playing = true;
+		}
+
+		SongPlayer.play = function(song){
+			if (currentSong !== song){
+				setSong(song);
+				playSong(song);
+			} else if (currentSong === song){
+				if (currentBuzzObject.isPaused()){
+					playSong(song);
+				}
+			}
+		};
+
+		SongPlayer.pause = function(song){
+			currentBuzzObject.pause();
+			song.playing = false;
+		};
+
+		return SongPlayer;
+	}
+
+	angular
+		.module('blocJams')
+		.factory('SongPlayer', SongPlayer);
+})();
